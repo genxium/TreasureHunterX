8b52329d7ba1b6c7c01f7254cc3170d2eefc0d66
ubuntu <ubuntu@localhost.localdomain>
2018-11-05 19:18:24 +0800

    Minor fix.

--- battle_srv/check_daemon.sh
+++ battle_srv/check_daemon.sh
@@ -3,6 +3,13 @@
 basedir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 PID_FILE="$basedir/treasure-hunter.pid"
-pid=$( cat "$PID_FILE" )
-
-echo $(ps aux | grep "$pid")
+if [ -f $PID_FILE ]; then
+  pid=$( cat "$PID_FILE" )
+  if [ -z $pid ]; then
+    echo "There's no pid stored in $PID_FILE."
+  else 
+    ps aux | grep "$pid"
+  fi
+else 
+  echo "There's no PidFile $PID_FILE."
+fi
