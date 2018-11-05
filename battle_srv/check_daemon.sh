66b8da0e7300b457f7f5ff5d94df06590ab72a5c
Wing <genxium@hotmail.com>
2018-11-05 18:51:16 +0800

    Updated deployment relevant scripts.

--- battle_srv/check_daemon.sh
+++ battle_srv/check_daemon.sh
@@ -5,5 +5,4 @@ basedir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 PID_FILE="$basedir/treasure-hunter.pid"
 pid=$( cat "$PID_FILE" )
 
-echo "killing process of id $pid"
-kill $pid 
+echo $(ps aux | grep "$pid")
--- battle_srv/start_daemon.sh
+++ battle_srv/start_daemon.sh
@@ -5,13 +5,17 @@ if [ $# -ne 1 ]; then
   exit 1
 fi
 
+basedir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
 OS_USER=$USER
 ServerEnv=$1
 LOG_PATH="/var/log/treasure-hunter.log"
 
-basedir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+# Make sure that the following "PidFile" is "git ignored".
+PID_FILE="$basedir/treasure-hunter.pid"
 
 sudo su - root -c "touch $LOG_PATH" 
 sudo su - root -c "chown $OS_USER:$OS_USER $LOG_PATH" 
 
 ServerEnv=$ServerEnv $basedir/server >$LOG_PATH 2>&1 &
+echo $! > $PID_FILE
