495fa75f8877e6d7275fce982fb1ba1028e73c4c
Wing <genxium@hotmail.com>
2018-11-20 15:44:48 +0800

    Minor fix.

--- battle_srv/proto_gen_shortcut.sh
+++ battle_srv/proto_gen_shortcut.sh
@@ -3,4 +3,4 @@
 basedir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 # You have to download the OS binary "protoc" from `https://developers.google.com/protocol-buffers/docs/downloads` and set it to $PATH appropriately.
 # You have to install `proto-gen-go` by `go get -u github.com/golang/protobuf/protoc-gen-go` as instructed in https://developers.google.com/protocol-buffers/docs/gotutorial too.
-protoc -I=$basedir/../models/ --go_out=$basedir/ room_downsync_frame.proto 
+protoc -I=$basedir/../frontend/assets/resources/pbfiles/ --go_out=$basedir/pb_output room_downsync_frame.proto 
