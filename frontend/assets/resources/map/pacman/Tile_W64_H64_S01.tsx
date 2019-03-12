a5004741c241ec352745386581fc8f5eeb0b7e62
kobako <zhuobinggang@gmail.com>
2019-03-12 16:42:58 +0800

    Add some assets for change map

---
+++ frontend/assets/resources/map/pacman/Tile_W64_H64_S01.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="Tile_W64_H64_S01" tilewidth="64" tileheight="64" tilecount="16" columns="4">
+ <image source="Tile_W64_H64_S01.png" width="256" height="256"/>
+ <tile id="12">
+  <properties>
+   <property name="type" value="lowScoreTreasure"/>
+  </properties>
+ </tile>
+ <tile id="13">
+  <properties>
+   <property name="type" value="guardTower"/>
+  </properties>
+ </tile>
+ <tile id="15">
+  <properties>
+   <property name="type" value="highScoreTreasure"/>
+  </properties>
+ </tile>
+</tileset>
