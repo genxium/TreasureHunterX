7976dfdc6bf2cb85973c484c51dee5c3265a77a9
genxium <genxium@hotmail.com>
2019-09-09 12:37:12 +0800

    Minor fix to prevent NullPtrException on frontend.

--- frontend/assets/scripts/CountdownToBeginGame.js
+++ frontend/assets/scripts/CountdownToBeginGame.js
@@ -9,24 +9,23 @@ cc.Class({
   },
 
   // LIFE-CYCLE CALLBACKS:
-
   onLoad() {
-
-
   },
+
   setData() {
     this.startedMillis = Date.now();
     this.durationMillis = 3000;  
   },
+
   update() {
     const currentGMTMillis = Date.now();
     const elapsedMillis = currentGMTMillis - this.startedMillis;
     let remainingMillis = this.durationMillis - elapsedMillis;
-    if(remainingMillis <= 0) {
+    if (remainingMillis <= 0) {
        remainingMillis = 0;  
     }
     let remaingHint = "" + Math.round(remainingMillis / 1000 ); 
-    if(remaingHint != this.countdownSeconds.string) {
+    if (remaingHint != this.countdownSeconds.string) {
       this.countdownSeconds.string = remaingHint;
     }
   }
